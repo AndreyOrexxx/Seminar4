27451895c1ae3fd0dd6790b2670af65c6a46214a
andrey_orexxx <KO.orexxx@yandex.ru>
2022-08-05 18:38:48 +0300

    Задача 25 решена

---
+++ Task25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 25
+// Напишите метод, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В.
+// 3, 5 -> 243
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А:");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число В:");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine("А в степени В равно:" + Math.Pow(A, B));
