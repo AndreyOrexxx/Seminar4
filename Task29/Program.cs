86faadd21db7c338c1a4af67469055508384ce1b
andrey_orexxx <KO.orexxx@yandex.ru>
2022-08-05 22:02:54 +0300

    Задача 29 решена

---
+++ Task29/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 29
+// Напишите метод, который задает массив из 8 элементов и выводит их на экран.
+
+// Числа задаю рандомно.
+
+void FillArrey(int[] colection)
+{
+    int length = colection.Length;
+    int index = 0;
+
+    while(index < length)
+    {
+        colection[index] = new Random().Next(1, 8);
+        colection[0]=8;
+        index++;
+    }
+}
+void PrintArrey(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int[] arrey = new int[8];
+
+FillArrey(arrey);
+PrintArrey(arrey);
+
